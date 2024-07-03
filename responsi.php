2574e7dd419a05771dca69169736f259644f43b1
Elysia <114798157+Ailiciya@users.noreply.github.com>
2024-07-03 12:15:13 +0700

    Add files via upload

---
+++ responsi.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsi - 4870</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="logo.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+                Responsi - 22.11.4870
+            </a>
+        </div>
+    </nav>
+    <div class="container-fluid mt-4">
+        <div class="row">
+            <div class="col">
+                <h2>Data Mahasiswa</h2>
+                <p class="lead">Filter jurusan</p>
+                <div class="mb-3">
+                    <select class="form-select" id="filterJurusan">
+                        <option value="">Semua Jurusan</option>
+                        <option value="teknik informatika">Teknik Informatika</option>
+                        <option value="sistem informasi">Sistem Informasi</option>
+                        <option value="sistem manajemen">Sistem Manajemen</option>
+                        <option value="ilmu komputer">Ilmu Komputer</option>
+                        <option value="teknik elektro">Teknik Elektro</option>
+                        <option value="manajemen bisnis">Manajemen Bisnis</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">NIM</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">No Telp</th>
+                    <th scope="col">Jenis Kelamin</th>
+                    <th scope="col">Jurusan</th>
+                    <th scope="col">Alamat</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Kiana Kaslana</td>
+                    <td>081234567891</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Informatika</td>
+                    <td>Surabaya</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Mei Raiden</td>
+                    <td>081234567892</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Informatika</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Bronya Zaychik</td>
+                    <td>081234567893</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Informatika</td>
+                    <td>Bandung</td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Himeko Murata</td>
+                    <td>081234567894</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Informatika</td>
+                    <td>Malang</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Fu Hua</td>
+                    <td>081234567895</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Informatika</td>
+                    <td>Bali</td>
+                </tr>
+                <tr>
+                    <th scope="row">6</th>
+                    <td>Yae Sakura</td>
+                    <td>081234567896</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Informasi</td>
+                    <td>Surabaya</td>
+                </tr>
+                <tr>
+                    <th scope="row">7</th>
+                    <td>Theresa Apocalypse</td>
+                    <td>081234567897</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Informasi</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <th scope="row">8</th>
+                    <td>Rita Rossweisse</td>
+                    <td>081234567898</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Informasi</td>
+                    <td>Bandung</td>
+                </tr>
+                <tr>
+                    <th scope="row">9</th>
+                    <td>Durandal</td>
+                    <td>081234567899</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Informasi</td>
+                    <td>Malang</td>
+                </tr>
+                <tr>
+                    <th scope="row">10</th>
+                    <td>Seele Vollerei</td>
+                    <td>081234567800</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Informasi</td>
+                    <td>Bali</td>
+                </tr>
+                <tr>
+                    <th scope="row">11</th>
+                    <td>Mobius</td>
+                    <td>081234567801</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Manajemen</td>
+                    <td>Surabaya</td>
+                </tr>
+                <tr>
+                    <th scope="row">12</th>
+                    <td>Elysia</td>
+                    <td>081234567802</td>
+                    <td>Perempuan</td>
+                    <td>Sistem Manajemen</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <th scope="row">13</th>
+                    <td>Kalpas</td>
+                    <td>081234567803</td>
+                    <td>Laki-laki</td>
+                    <td>Sistem Manajemen</td>
+                    <td>Bandung</td>
+                </tr>
+                <tr>
+                    <th scope="row">14</th>
+                    <td>Kevin</td>
+                    <td>081234567804</td>
+                    <td>Laki-laki</td>
+                    <td>Sistem Manajemen</td>
+                    <td>Malang</td>
+                </tr>
+                <tr>
+                    <th scope="row">15</th>
+                    <td>Su</td>
+                    <td>081234567805</td>
+                    <td>Laki-laki</td>
+                    <td>Sistem Manajemen</td>
+                    <td>Bali</td>
+                </tr>
+                <tr>
+                    <th scope="row">16</th>
+                    <td>Aponia</td>
+                    <td>081234567806</td>
+                    <td>Perempuan</td>
+                    <td>Ilmu Komputer</td>
+                    <td>Surabaya</td>
+                </tr>
+                <tr>
+                    <th scope="row">17</th>
+                    <td>Griseo</td>
+                    <td>081234567807</td>
+                    <td>Perempuan</td>
+                    <td>Ilmu Komputer</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <th scope="row">18</th>
+                    <td>Hua</td>
+                    <td>081234567808</td>
+                    <td>Perempuan</td>
+                    <td>Ilmu Komputer</td>
+                    <td>Bandung</td>
+                </tr>
+                <tr>
+                    <th scope="row">19</th>
+                    <td>Pardofelis</td>
+                    <td>081234567809</td>
+                    <td>Perempuan</td>
+                    <td>Ilmu Komputer</td>
+                    <td>Malang</td>
+                </tr>
+                <tr>
+                    <th scope="row">20</th>
+                    <td>Vill-V</td>
+                    <td>081234567810</td>
+                    <td>Perempuan</td>
+                    <td>Ilmu Komputer</td>
+                    <td>Bali</td>
+                </tr>
+                <tr>
+                    <th scope="row">21</th>
+                    <td>Carole Pepper</td>
+                    <td>081234567811</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Elektro</td>
+                    <td>Surabaya</td>
+                </tr>
+                <tr>
+                    <th scope="row">22</th>
+                    <td>Phoebe</td>
+                    <td>081234567812</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Elektro</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <th scope="row">23</th>
+                    <td>Hua Ling</td>
+                    <td>081234567813</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Elektro</td>
+                    <td>Bandung</td>
+                </tr>
+                <tr>
+                    <th scope="row">24</th>
+                    <td>Sakura Haruno</td>
+                    <td>081234567814</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Elektro</td>
+                    <td>Malang</td>
+                </tr>
+                <tr>
+                    <th scope="row">25</th>
+                    <td>Benni</td>
+                    <td>081234567815</td>
+                    <td>Perempuan</td>
+                    <td>Teknik Elektro</td>
+                    <td>Bali</td>
+                </tr>
+                <tr>
+                    <th scope="row">26</th>
+                    <td>Adam</td>
+                    <td>081234567816</td>
+                    <td>Laki-laki</td>
+                    <td>Manajemen Bisnis</td>
+                    <td>Surabaya</td>
+                </tr>
+                <tr>
+                    <th scope="row">27</th>
+                    <td>Alan</td>
+                    <td>081234567817</td>
+                    <td>Laki-laki</td>
+                    <td>Manajemen Bisnis</td>
+                    <td>Jakarta</td>
+                </tr>
+                <tr>
+                    <th scope="row">28</th>
+                    <td>Blake</td>
+                    <td>081234567818</td>
+                    <td>Laki-laki</td>
+                    <td>Manajemen Bisnis</td>
+                    <td>Bandung</td>
+                </tr>
+                <tr>
+                    <th scope="row">29</th>
+                    <td>Caden</td>
+                    <td>081234567819</td>
+                    <td>Laki-laki</td>
+                    <td>Manajemen Bisnis</td>
+                    <td>Malang</td>
+                </tr>
+                <tr>
+                    <th scope="row">30</th>
+                    <td>Dylan</td>
+                    <td>081234567820</td>
+                    <td>Laki-laki</td>
+                    <td>Manajemen Bisnis</td>
+                    <td>Bali</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <script>
+        document.getElementById('filterJurusan').addEventListener('change', function() {
+            const filter = this.value.toLowerCase();
+            const rows = document.querySelectorAll('tbody tr');
+            rows.forEach(row => {
+                const jurusan = row.cells[4].textContent.toLowerCase();
+                if (filter === "" || jurusan.includes(filter)) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        });
+    </script>
+</body>
+</html>
